76f3207ea5cef902d1375865aa7ab83dab100a0c
mengzhey <106492397+mengzhey@users.noreply.github.com>
2022-12-09 15:05:04 -0500

    12/09

---
+++ js/tooltip.js
@@ -0,0 +1,39 @@
+tippy('#tip-image-1', {
+    delay: 200,
+    content: '<img id="icon-tip" src="asset/chef.png" style="width:50px;"><p style="font-size:14px;line-height:20px;height:210px;width:200px;">Increase baking time: If you did everything right, maybe the baking time was not enough. Do not grease: For the same reason, you do not need to grease the cake pan.</p>',
+    theme: 'tomato',
+    animation: 'shift-toward',
+    placement: 'right',
+    interactive: true,
+    allowHTML: true,
+  });
+
+  tippy('#tip-image-2', {
+    delay: 200,
+    content: '<img id="icon-tip" src="asset/chef.png" style="width:50px;"><p style="font-size:14px;line-height:20px;height:210px;width:200px;">Beat egg whites correctly: When egg whites are under-beaten, the small air bubbles cannot sustain as the temperature goes up. As a result, small air bubbles turn into one bigger hole.</p>',
+    theme: 'tomato',
+    animation: 'shift-toward',
+    placement: 'right',
+    interactive: true,
+    allowHTML: true,
+  });
+
+  tippy('#tip-image-3', {
+    delay: 200,
+    content: '<img id="icon-tip" src="asset/chef.png" style="width:50px;"><p style="font-size:14px;line-height:20px;height:210px;width:200px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
+    theme: 'tomato',
+    animation: 'shift-toward',
+    placement: 'right',
+    interactive: true,
+    allowHTML: true,
+  });
+
+  tippy('#tip-image-4', {
+    delay: 200,
+    content: '<img id="icon-tip" src="asset/chef.png" style="width:50px;"><p style="font-size:14px;line-height:20px;height:210px;width:200px;">Prevent air pockets: The air pockets may be created when you pour the batter into the chiffon cake pan. Make sure to pour the cake batter all at once in one location. Also, run the wooden skewer in the batter a few times and gently tap the chiffon cake pan against the kitchen counter</p>',
+    theme: 'tomato',
+    animation: 'shift-toward',
+    placement: 'right',
+    interactive: true,
+    allowHTML: true,
+  });
