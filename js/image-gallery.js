c745581bb622aab0c497c9a5a2985f45cb43ae40
mengzhey <106492397+mengzhey@users.noreply.github.com>
2023-04-17 01:04:59 -0400

    update

--- js/image-gallery.js
+++ js/image-gallery.js
@@ -1,26 +1,9 @@
-const swiper1 = new Swiper('.mySwiper1', {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-  
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-  
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
+const swiper1 = new Swiper(".mySwiper1", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
   
   const swiper2 = new Swiper('.mySwiper2', {
     cssMode: true,
